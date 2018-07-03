c5aa7716fe0c61bdb502221c2f5f2f9a7b439208
hshg <jjxiong.hk@gmail.com>
2018-07-03 14:23:50 +0800

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,11 @@
+    long long trailingZeros(long long n) {
+        // Initialize result
+        long long count = 0;
+     
+        // Keep dividing n by powers of 
+        // 5 and update count
+        for (long long i = 5; n / i >= 1; i *= 5)
+            count += n / i;
+     
+        return count;
+    }
